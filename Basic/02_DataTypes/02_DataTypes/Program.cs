0344aa7da3615fa66c1bc621e9045ad824d19fcf
Tento <regartento@gmail.com>
2026-02-28 23:52:23 +0700

    add datatypes & convert data types

---
+++ Basic/02_DataTypes/02_DataTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿int myInt = 10;
+double myDouble = 10.5;
+string myString = "Tento";
+bool myBool = true;
+
+Console.WriteLine("Int:" + myInt);
+Console.WriteLine("Double:" + myDouble);
+
+Console.WriteLine("Doube to Int:" + Convert.ToInt64(myDouble));
+Console.WriteLine("Bool to String:" + Convert.ToString(myBool));
