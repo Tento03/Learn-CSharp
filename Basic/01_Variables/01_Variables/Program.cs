f0be4d3e4fc6bdbab8b0a12fb66c2d11597f7503
Tento <regartento@gmail.com>
2026-02-28 23:05:12 +0700

    add 01_variables

---
+++ Basic/01_Variables/01_Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿int myInt = 2;
+double myDouble = 2.5D;
+string myString = "Tento";
+char myChar = 'S';
+bool myBool = true;
+
+Console.WriteLine("Int:" + myInt);
+Console.WriteLine("Double:" + myDouble);
+Console.WriteLine("String:" + myString);
+Console.WriteLine("Char:" + myChar);
+Console.WriteLine("Bool:" + myBool);
+
+int x, y, z;
+x = y = z = 10;
+Console.WriteLine(x + y + z);
