e746091451090e95dc52df7d361a591f9225d34d
Tento <regartento@gmail.com>
2026-03-01 00:16:07 +0700

    add input userr

---
+++ Basic/03_UserInput/03_UserInput/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Type your name: ");
+string name = Console.ReadLine();
+
+Console.Write("Type your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your name is: " + name);
+Console.WriteLine("Your age is: " + age);
