ea99149bece863de1c33c702d3aa15852b999e19
Tento <regartento@gmail.com>
2026-03-01 14:32:34 +0700

    add 04 if else

---
+++ Basic/04_IfElse/04_IfElse/Program.cs
@@ -0,0 +1,18 @@
+﻿string color = "red";
+
+if (color == "red")
+{
+    Console.WriteLine("Stop");
+} 
+else if (color=="yellow")
+{
+    Console.WriteLine("Be careful");
+}
+else if (color == "green")
+{
+    Console.WriteLine("Go");
+}
+
+int myNum = 2;
+string result = (myNum == 2) ? "Yes" : "No";
+Console.WriteLine("Result: " + result);
