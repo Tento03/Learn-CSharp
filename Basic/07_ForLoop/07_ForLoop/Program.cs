9b573c0a45788cc14fc33df8f556961bca131986
Tento <regartento@gmail.com>
2026-03-01 15:02:06 +0700

    add 07 for loop

---
+++ Basic/07_ForLoop/07_ForLoop/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i <= 5; i++)
+{
+    Console.WriteLine("i: " + i);
+}
+
+Console.WriteLine("======================");
+
+string[] playtime = { "Player", "Poppy", "Huggy", "Kissy", "Doey", "Prototype" };
+foreach (var chars in playtime)
+{
+    Console.WriteLine("Character in Poppy Playtime:" + chars);
+}
