b39c9130bc7904fb3b227811dbf312de842be7f2
Tento <regartento@gmail.com>
2026-03-02 00:18:19 +0700

    add 03 polymorphism

---
+++ OOP/04_Polymorphism/04_Polymorphism/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("The Animals have different sound based on their kind");
+    }
+}
+
+public class Dog:Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("guk guk guk");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Meow meow meow");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.Sound();
+        animal2.Sound();
+    }
+}
