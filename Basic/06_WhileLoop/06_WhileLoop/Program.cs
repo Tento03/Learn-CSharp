45adf43b9e54400bb8aeefa664b23518b34603a1
Tento <regartento@gmail.com>
2026-03-01 14:50:36 +0700

    add 06 While loop

---
+++ Basic/06_WhileLoop/06_WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = 0;
+int j = 0;
+
+while (i<=5)
+{
+    Console.WriteLine("i:" + i);
+    i++;
+}
+
+Console.WriteLine("==================");
+
+do
+{
+    Console.WriteLine("Do while:" + j);
+    j++;
+}
+while (j<=5);
