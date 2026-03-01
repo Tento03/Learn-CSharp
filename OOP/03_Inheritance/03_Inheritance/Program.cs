859e18511ad1eb22adb29107c320a0f2430237b2
Tento <regartento@gmail.com>
2026-03-02 00:09:01 +0700

    add 03 Inheritance

---
+++ OOP/03_Inheritance/03_Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+
+class Dog : Animal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Sound();
+    }
+}
