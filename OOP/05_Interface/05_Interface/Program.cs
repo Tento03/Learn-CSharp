801c7db2b4b718359ffe120dfd4b0f0db3cbe1e0
Tento <regartento@gmail.com>
2026-03-02 00:33:37 +0700

    add 05 interface

---
+++ OOP/05_Interface/05_Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿interface IPayment
+{
+    void Process();
+}
+
+
+
+public class TransferBank:IPayment
+{
+    public void Process()
+    {
+        Console.WriteLine("Transfer bank is processing");
+    }
+}
+
+public class EWallet : IPayment
+{
+    public void Process()
+    {
+        Console.WriteLine("E wallet is processing");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        IPayment payment1 = new TransferBank();
+        IPayment payment2 = new EWallet();
+
+        payment1.Process();
+        payment2.Process();
+    }
+}
