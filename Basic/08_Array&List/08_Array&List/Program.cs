dcffe48bd1d6f373398ee2dc34a37ec17d269388
Tento <regartento@gmail.com>
2026-03-01 15:16:15 +0700

    add 08 array & list

---
+++ Basic/08_Array&List/08_Array&List/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] cars = { "Avanza", "Bmw", "Ferrari" };
+
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine("Car loop regular:" + cars[i]);
+}
+
+Console.WriteLine("====================================");
+
+foreach (var car in cars)
+{
+    Console.WriteLine("Car looped with foreach:" + car);
+}
+
+Console.WriteLine("====================================");
+
+//List
+
+List<string> characters = new List<string>();
+characters.Add("Player");
+characters.Add("Poppy");
+characters.Add("Doey");
+characters.Add("Protoype");
+
+foreach(var character in characters)
+{
+    Console.WriteLine("Char in Poppy Playtime:" + character);
+}
+
+characters.Remove("Prototype");
